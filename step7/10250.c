e3dc4c5d6ecb1b46daa50c2abd56930390f4a1ce
c-jinwoo <jihn011258@naver.com>
2023-02-20 00:02:48 +0900

    2023.02.20

--- step7/10250.c
+++ step7/10250.c
@@ -1,6 +1,6 @@
 /*
  *  문제 : ACM 호텔
- *  날짜 : 2023년 2월 20일 
+ *  날짜 : 2023년 2월 19일 
  */
 
 #include <stdio.h>
@@ -16,17 +16,10 @@ int main(void){
 
         int floor = 0, room = 1;
         
-        floor = n % h;                  // 배정 층 = 전체 층으로 나눈 나머지
-        room += n / h;                  // 배정 호실 = 전체 층으로 나눈 몫만큼 오른쪽 1칸 이동
-
-        printf("%d", floor);
-    
-        if(room < 10){
-            printf("0%d\n", room);
-        }
-        else{
-            printf("%d\n", room);
-        }
+		floor = (n - 1) % h + 1;
+		room += (n - 1) / h;
+	
+        printf("%d%02d\n", floor, room);
     }
 
     return 0;
