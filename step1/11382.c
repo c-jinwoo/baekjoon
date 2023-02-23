0767a96c35a12dd3a4fb03949dd50a51d116b7d3
c-jinwoo <jihn011258@naver.com>
2023-02-23 18:34:04 +0900

    2023.02.23

---
+++ step1/11382.c
@@ -0,0 +1,16 @@
+/*
+ *  문제 : 꼬마 정민
+ *  날짜 : 2023년 2월 23일 
+ */
+
+#include <stdio.h>
+
+int main(void){
+    long long a, b, c;
+
+    scanf("%lld %lld %lld", &a, &b, &c);
+    
+    printf("%lld\n", a + b + c);
+
+    return 0;
+}
